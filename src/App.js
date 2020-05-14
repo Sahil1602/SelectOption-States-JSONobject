3fd193e71e1fbbf84170852faf5d631102997ade
Sahil1602 <sb160295@gmail.com>
2020-05-14 16:53:26 +0530

    my commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Option from './Option'
+import States from './StateDistrict'
+class App extends React.Component {
+  state={
+    stateOption: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+
+  componentDidMount(){
+    console.log(States)
+    this.setState({stateOption: States})
+    console.log(this.state.stateOption)
+  }
+
+  render(){
+    // console.log('loaded')
+      const states = this.state.stateOption.map(choice =>{
+      return <Option thisState= {choice.state} />
+    })
+    return (
+      <div className="App">
+        <select className='selectStates'>
+          {states}
+        </select>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
---
+++ src/Option.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class optStates extends React.Component {
+    render(){
+        return(
+        <option>{this.props.thisState}</option>
+        )
+    }
+    
+}
+
+export default optStates;
